b637ccc9d634f1353a8dd348860c34a3ae1a819a
oomathurman <atari6@gmail.com>
2024-12-09 20:43:48 +0000

    /32

--- okta_ips.sh
+++ okta_ips.sh
@@ -2,4 +2,4 @@
 
 URL=https://s3.amazonaws.com/okta-ip-ranges/ip_ranges.json
 
-curl -s $URL | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' > okta_ips.txt
+curl -s $URL | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}/32' > okta_ips.txt
